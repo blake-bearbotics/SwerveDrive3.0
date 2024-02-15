66a09c07d5d01363c20272320d225059ee67f677
gflikke24 <156116394+gflikke24@users.noreply.github.com>
2024-02-15 16:32:12 -0600

    intake motor?

---
+++ SwerveDrive3.0/SwerveDrive3.0/src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+
+public class Intake {
+    private final CANSparkMax m_intakeMotor = new CANSparkMax(1, MotorType.kBrushless); // change devideID to whatever port it is
+    
+    public void runIntake() {
+        m_intakeMotor.set(0.5);
+    }
+
+    public void stopIntake() {
+        m_intakeMotor.set(0.0);
+    }
+}
