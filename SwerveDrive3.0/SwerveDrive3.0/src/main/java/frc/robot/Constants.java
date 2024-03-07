bbd52ec4be9d41fbd5cf4089b0f522b40dd7388d
zosialazarewicz <zlazarewicz24@blakeschool.org>
2024-03-07 15:50:38 -0600

    zero wheels?

--- SwerveDrive3.0/SwerveDrive3.0/src/main/java/frc/robot/Constants.java
+++ SwerveDrive3.0/SwerveDrive3.0/src/main/java/frc/robot/Constants.java
@@ -16,39 +16,74 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
     
-    public static final double kWheelBase = 0.5588;
-    public static final double kTrackWidth = 0.5588;
-    
+    //Drivebase Constants
+    public static final double kWheelBase = 0.5588; //meters
+    public static final double kTrackWidth = 0.5588; //meters
+    public static final double kWheelRadius = 0.0508; //meters
+    public static final double kDriveMotorGearRatio = 6.75; //6.75 motor rot = 1 wheel rot
+    public static final double kTurningMotorGearRatio = 150/7; //150/7 motor rot = 1 wheel rot
+
+    //Drive Motor IDs
     public static final int frontLeftDriveMotorChannel = 4;
     public static final int frontRightDriveMotorChannel = 1;
     public static final int backLeftDriveMotorChannel = 2;
     public static final int backRightDriveMotorChannel = 3;
 
+    //Turning Motor IDs
     public static final int frontLeftTurningMotorChannel = 14;
     public static final int frontRightTurningMotorChannel = 11;
     public static final int backLeftTurningMotorChannel = 12;
     public static final int backRightTurningMotorChannel = 13;
 
+    //Encoder IDs
     public static final int frontLeftEncoderChannel = 24;
     public static final int frontRightEncoderChannel = 21;
     public static final int backLeftEncoderChannel = 22;
     public static final int backRightEncoderChannel = 23;
 
-    //must put in rotations
-    public static final double frontLeftEncoderOffset = -0.357422;//-6.504; 
-    public static final double frontRightEncoderOffset = 0.168213;//342.334;
-    public static final double backLeftEncoderOffset = -0.367920;//53.174;
-    public static final double backRightEncoderOffset = -0.447266;//188.789;
+    //Swerve CANCoder Sensor offsets
+    //CHANGE TO 0 first, reset the sensor, 
+    //PHYSICALLY zero out the motor 
+    //place the OPPOSITE of the value
+    public static double frontLeftEncoderOffset = 17.666;
+    public static double frontRightEncoderOffset = 57.744;
+    public static double backLeftEncoderOffset = -165.059;
+    public static double backRightEncoderOffset = 67.061;
 
+    //Constants for conversion maths
+    public static final double secondsPer100ms = .1;
+    public static final double turningEncoderTicksPerRotation = 4096;
+    public static final double turningEncoderRotationsPerTick= 360/turningEncoderTicksPerRotation;
+    public static final double TICKSperTALONFX_Rotation = 2048;
+    //public static final double DRIVE_MOTOR_TICKSperREVOLUTION = DRIVE_MOTOR_GEARING*TICKSperTALONFX_Rotation;
+    public static final double metersPerWheelRevolution = 2*Math.PI*kWheelRadius;
+    public static final double metersPerRobotRevolution =  2*Math.PI*(kWheelBase/2*Math.sqrt(2));
+    public static final double maxSpeedMetersPerSecond = 0.0; //figure this out later
+    public static final double maxSpeedRadiansPerSecond = maxSpeedMetersPerSecond/metersPerRobotRevolution*(2*Math.PI);
+    //public static final double TICKSperTALONFX_DEGREE = TICKSperTALONFX_Rotation*STEERING_MOTOR_GEARING/360;
+
+    //Swerve Drive Constants
+    public static final boolean defaultFieldRelativeDrive = true;
+    public static final double robotHoldAngleKP = 10; //Start at .7 and see where you go from there
+    public static final boolean defaultHoldRobotAngle = true;
+	  public static final double defaultHoldRobotAngleSetpoint = 0; 
+
+    //Turning PID values
     public static final double[] frontLeftEncoderPID = {0.0, 0.0, 0.0};
     public static final double[] frontRightEncoderPID = {0.0, 0.0, 0.0};
     public static final double[] backLeftEncoderPID = {0.0, 0.0, 0.0};
     public static final double[] backRightEncoderPID = {0.0, 0.0, 0.0};
 
+    //Turning FeedForward Values
     public static final double[] frontLeftTurningMotorFeedforward = {0.0, 0.0}; //start ~2.5
     public static final double[] frontRightTurningMotorFeedforward = {0.0, 0.0};
     public static final double[] backLeftTurningMotorFeedforward = {0.0, 0.0};
     public static final double[] backRightTurningMotorFeedforward = {0.0, 0.0};
+
+    //CTRE CAN-based constants (shouldn't need to change these)
+    public static final int kDefaultPIDSlotID = 0;
+    public static final int kDefaultTimeout = 30;//milliseconds
+    public static final int kDefaultClosedLoopError = 1; //degrees 
 
   }
 }
